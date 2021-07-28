7b5bcd2b74a23a8459c23f3ff22ec933b6d1b814
strixsoul <rsilveira136@gmail.com>
2021-07-28 17:12:14 -0300

    completed repositories with raw queries, queryBuilder and ORM

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -14,17 +14,24 @@ export class UsersRepository implements IUsersRepository {
   async findUserWithGamesById({
     user_id,
   }: IFindUserWithGamesDTO): Promise<User> {
-    // Complete usando ORM
+    const userWithGames = (await this.repository.findOne(user_id, {
+      relations: ["games"],
+    })) as User;
+
+    return userWithGames;
   }
 
   async findAllUsersOrderedByFirstName(): Promise<User[]> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(`SELECT * FROM USERS ORDER BY FIRST_NAME ASC`); // Complete usando raw query
   }
 
   async findUserByFullName({
     first_name,
     last_name,
   }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
-    return this.repository.query(); // Complete usando raw query
+    return this.repository.query(
+      `SELECT * FROM USERS WHERE Lower(FIRST_NAME) = Lower($1) AND Lower(LAST_NAME) = Lower($2)`,
+      [first_name, last_name]
+    ); // Complete usando raw query
   }
 }
